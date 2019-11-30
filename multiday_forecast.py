1a548541dbc231b8f1931572b0d9ea2f50007a55
Cameron Young <cameron.young.16@cnu.edu>
2019-11-29 21:28:43 -0500

    initial commit

---
+++ multiday_forecast.py
@@ -0,0 +1,385 @@
+from datetime import datetime, timedelta
+import sqlite3
+from math import e as E
+from math import sqrt
+import json
+
+
+class MultidayForecast():
+    def __init__(self, forecast=None):
+        self.cnx = sqlite3.connect(":memory:")
+        self.cursor = self.cnx.cursor()
+        self.cursor.execute('''CREATE TABLE weather 
+            (_id INTEGER PRIMARY KEY AUTOINCREMENT, dt timestamp, temp_avg FLOAT, 
+            temp_hi FLOAT, temp_lo FLOAT, humidity FLOAT, clouds INT, wind INT, rain FLOAT, 
+            snow FLOAT, wind_chill FLOAT, heat_index FLOAT);''')
+        
+        self.cnx.create_function(
+            "APPARENT_TEMPERATURE", 3,
+            self.__apparent_temp
+        )
+
+        self.cnx.commit()
+
+        if forecast is not None:
+            self.populate(forecast)
+
+    def __del__(self):
+        self.cursor.close()
+        self.cnx.close()
+
+        del self.cursor
+        del self.cnx
+
+    def __repr__(self):
+        try:
+            query = "SELECT * FROM weather;"
+            results = self.cursor.execute(query).fetchall()
+            return "\n".join([r for r in results])
+
+        except Exception as e:
+            print(e)
+
+    def populate(self, forecast):
+        days = forecast["list"]
+        try:
+            for data in days:
+
+                date = datetime.strptime(
+                    data['dt_txt'], "%Y-%m-%d %X").timestamp()
+                temp_avg = data['main']['temp']
+                temp_hi = data['main']['temp_max']
+                temp_lo = data['main']['temp_min']
+                humidity = data['main']['humidity']
+                clouds = data['clouds']['all']
+                wind = data['wind']['speed']
+                rain = 0 if 'rain' not in data else data['rain']
+                if isinstance(rain, dict):
+                    rain = 0 if '3h' not in rain else rain['3h']
+                snow = 0 if 'snow' not in data else data['snow']
+                if isinstance(snow, dict):
+                    snow = 0 if '3h' not in snow else snow['3h']
+                wind_chill = 0  # create
+                heat_index = 0  # create
+
+                query = ''' 
+                    INSERT INTO weather(
+                        dt, temp_avg, temp_hi, temp_lo, humidity,
+                        clouds, wind, rain, snow, wind_chill, heat_index
+                    ) VALUES (
+                        {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+                    );
+                '''.format(date, temp_avg, temp_hi, temp_lo, humidity, clouds, wind, 
+                    rain, snow, wind_chill, heat_index)
+
+                self.cursor.execute(query)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            self.cnx.commit()
+
+    def __apparent_temp(self, T, RH, W):
+        HI = 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094))
+        HI = (HI + T) / 2
+
+        if HI >= 80:
+            ADJUSTMENT = 0
+            if RH <= 13 and 80 >= T >= 112:
+                ADJUSTMENT = ((13-RH)/4) * sqrt((17-abs(T-95.))/17)
+            elif RH >= 85 and 80 >= T >= 87:
+                ADJUSTMENT = ((RH-85)/10) * ((87-T)/5)
+
+            HI = -42.379 + 2.04901523*T + 10.14333127*RH - .22475541*T*RH - .00683783*T * \
+                T - .05481717*RH*RH + .00122874*T*T*RH + .00085282*T*RH*RH - .00000199*T*T*RH*RH
+            HI -= ADJUSTMENT
+
+        return HI - (1.072 * W)
+
+    def __time_range(self, start_dt, end_dt):
+        start_dt = datetime.today().timestamp() if start_dt is None \
+            else start_dt.timestamp()
+
+        end_dt = (datetime.today() + timedelta(days=5)).timestamp() if end_dt is None \
+            else end_dt.timestamp()
+
+        return (start_dt, end_dt)
+
+    def __find_avg(self, start_dt, end_dt, field_name):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = '''
+                SELECT AVG({})
+                FROM weather
+                WHERE dt BETWEEN {} AND {}
+            '''.format(field_name, start_dt, end_dt)
+
+            result = self.cnx.execute(query).fetchone()
+            return result[0]
+
+        except Exception as e:
+            print(e)
+
+    def average_rain(self, start_dt=None, end_dt=None):
+        return self.__find_avg(start_dt, end_dt, 'rain')
+
+    def average_snow(self, start_dt=None, end_dt=None):
+        return self.__find_avg(start_dt, end_dt, 'snow')
+
+    def average_temp(self, start_dt=None, end_dt=None):
+        return self.__find_avg(start_dt, end_dt, 'temp_avg')
+
+    def highest_temp(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = f"SELECT DATETIME(dt,\'unixepoch\'),MAX(temp_hi) FROM weather WHERE dt BETWEEN {start_dt} AND {end_dt};"
+            results = self.cursor.execute(query).fetchone()
+            return {
+                "dt": results[0],
+                "temp":results[1]
+            }
+
+        except Exception as e:
+            print(e)
+
+    def lowest_temp(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = f"SELECT DATETIME(dt,\'unixepoch\'),MIN(temp_lo) FROM weather WHERE dt BETWEEN {start_dt} AND {end_dt};"
+            results = self.cursor.execute(query).fetchone()
+            return {
+                "dt": results[0],
+                "temp":results[1]
+            }
+
+        except Exception as e:
+            print(e)
+
+    def average_temp_on(self, date):
+        try:
+            query = '''
+                SELECT DATE(dt,\'unixepoch\'),AVG(temp_avg)
+                FROM weather
+                WHERE DATE(dt,\'unixepoch\')=DATE({},\'unixepoch\')
+            '''.format(date.timestamp())
+
+            result = self.cursor.execute(query).fetchone()
+            return {"dt": result[0], "temp": result[1]}
+
+        except Exception as e:
+            print(e)
+
+    def forecast_on(self, date):
+        try:
+            query = '''
+                SELECT * FROM weather
+                WHERE DATE(dt,\'unixepoch\')=DATE({},\'unixepoch\')
+            '''.format(date.timestamp())
+
+            results = self.cursor.execute(query).fetchall()
+            return [
+                {
+                    "dt": res[1],
+                    "temp_avg": res[2],
+                    "temp_hi": res[3],
+                    "temp_lo": res[4],
+                    "humidity": res[5],
+                    "clouds": res[6],
+                    "wind": res[7],
+                    "rain": res[8],
+                    "snow": res[9],
+                    "wind_chill": res[10],
+                    "heat_index": res[11],
+                } for res in results
+            ]
+
+        except Exception as e:
+            print(e)
+
+    def rainy_days(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        query = '''
+            SELECT 
+                DATE(dt,\'unixepoch\'),
+                SUM(rain)
+            FROM weather
+            WHERE 
+                (dt BETWEEN {} AND {})
+                AND rain > 0
+            GROUP BY DATE(dt,\'unixepoch\')
+        '''.format(start_dt, end_dt)
+
+        results = self.cursor.execute(query).fetchall()
+        return [
+            {
+                "dt": result[0],
+                "rain": result[1]
+            } for result in results
+        ]
+    
+    def snowy_days(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        query = '''
+            SELECT 
+                DATE(dt,\'unixepoch\'),
+                SUM(snow)
+            FROM weather
+            WHERE 
+                (dt BETWEEN {} AND {})
+                AND snow > 0
+            GROUP BY DATE(dt,\'unixepoch\')
+        '''.format(start_dt, end_dt)
+
+        results = self.cursor.execute(query).fetchall()
+        return [
+            {
+                "dt": result[0],
+                "snow": result[1]
+            } for result in results
+        ]
+
+    def rainiest_day(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = '''
+                with sums as (
+                    select 
+                        DATE(dt,\'unixepoch\') as dt, 
+                        SUM(rain) as rain 
+                    FROM weather 
+                    WHERE dt BETWEEN {} and {}
+                    GROUP BY DATE(dt,\'unixepoch\')
+                )
+                SELECT dt,MAX(rain) rain FROM sums
+            '''.format(start_dt, end_dt)
+
+            result = self.cursor.execute(query).fetchone()
+            return {
+                "dt": result[0],
+                "rain": result[1]
+            }
+
+        except Exception as e:
+            print(e)
+
+    def snowiest_day(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = '''
+                with sums as (
+                    select 
+                        DATE(dt,\'unixepoch\') as dt, 
+                        SUM(snow) as snow 
+                    FROM weather 
+                    WHERE dt BETWEEN {} and {}
+                    GROUP BY DATE(dt,\'unixepoch\')
+                )
+                SELECT dt,MAX(snow) snow FROM sums
+            '''.format(start_dt, end_dt)
+
+            results = list(self.cursor.execute(query))[0]
+            return {
+                "dt": results[0],
+                "snow": results[1]
+            }
+
+        except Exception as e:
+            print(e)
+
+    def highest_apparent_temp(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = '''
+                WITH temps AS (
+                    SELECT
+                        AVG(temp_avg) AS temp_avg, 
+                        AVG(humidity) AS humidity, 
+                        AVG(wind) AS wind,
+                        DATE(dt,\'unixepoch\') AS dt
+                    FROM weather
+                    WHERE dt BETWEEN {} AND {}
+                    GROUP BY DATE(dt, \'unixepoch\')
+                ), apts AS (
+                    SELECT 
+                        APPARENT_TEMPERATURE(temp_avg, humidity, wind) AS apt, 
+                        dt 
+                    FROM temps
+                )
+                SELECT dt, MAX(apt) FROM apts
+            '''.format(start_dt, end_dt)
+
+            result = self.cursor.execute(query).fetchone()
+            return {
+                "dt": result[0],
+                "temp": result[1]
+            }
+
+        except Exception as e:
+            print(e)
+
+    def lowest_apparent_temp(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        try:
+            query = '''
+                WITH temps AS (
+                    SELECT
+                        AVG(temp_avg) AS temp_avg, 
+                        AVG(humidity) AS humidity, 
+                        AVG(wind) AS wind,
+                        DATE(dt,\'unixepoch\') AS dt
+                    FROM weather
+                    WHERE dt BETWEEN {} AND {}
+                    GROUP BY DATE(dt, \'unixepoch\')
+                ), apts AS (
+                    SELECT 
+                        APPARENT_TEMPERATURE(temp_avg, humidity, wind) AS apt, 
+                        dt 
+                    FROM temps
+                )
+                SELECT dt, MIN(apt) FROM apts
+            '''.format(start_dt, end_dt)
+
+            result = self.cursor.execute(query).fetchone()
+            return {
+                "dt": result[0],
+                "temp": result[1]
+            }
+
+        except Exception as e:
+            print(e)
+    
+    def average_apparent_temp(self, start_dt=None, end_dt=None):
+        start_dt, end_dt = self.__time_range(start_dt, end_dt)
+        query = '''
+            WITH temps AS (
+                SELECT 
+                    APPARENT_TEMPERATURE(temp_avg, humidity, wind) as apt
+                FROM weather
+                WHERE dt BETWEEN {} AND {}
+            )
+            SELECT AVG(apt) from temps
+        '''.format(start_dt, end_dt)
+
+        result = self.cnx.execute(query).fetchone()
+        return {"temp": result[0]}
+
+    def highest_temp_on(self, date):
+        query = '''
+            SELECT DATE(dt,\'unixepoch\'),MAX(temp_hi)
+            FROM weather
+            WHERE DATE(dt,\'unixepoch\')=DATE({},\'unixepoch\');
+        '''.format(date.timestamp())
+
+        result = self.cnx.execute(query).fetchone()
+        return {"dt":result[0],"temp":result[1]}
+    
+    def lowest_temp_on(self, date):
+        query = '''
+            SELECT DATE(dt,\'unixepoch\'),MIN(temp_hi)
+            FROM weather
+            WHERE DATE(dt,\'unixepoch\')=DATE({},\'unixepoch\');
+        '''.format(date.timestamp())
+
+        result = self.cnx.execute(query).fetchone()
+        return {"dt":result[0],"temp":result[1]}
